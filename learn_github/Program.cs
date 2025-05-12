77a3ec31bc77586336f7db2cae2b558e20aaee78
srullsim <simchonisrael@gmail.com>
2025-05-12 15:41:31 +0300

    build the body of the program

--- learn_github/Program.cs
+++ learn_github/Program.cs
@@ -1,2 +1,107 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+class Program
+{
+    //  build a menu 
+    static void ValidInput()
+    {
+        Console.WriteLine("");
+    }
+
+
+    // keep the program alive 
+    static void WriteMenu(string menuName)
+    {
+        Console.WriteLine("");
+    }
+
+
+    // cases 
+    static void CasesToChoise()
+    {
+        Console.WriteLine("");
+    }
+
+
+    // get new input - case 1
+    static List<string> GetNewList()
+    {
+        return new List<string>();
+    }
+
+
+    // write input - case 2
+    static void PrintInput(List<string> input)
+    {
+        Console.WriteLine(input);
+    }
+
+
+    // print revers - case 3
+    static void PrintRevers(List<string> revers)
+    {
+        Console.WriteLine("");
+    }
+
+
+    // print sort - case 4
+    static void PrintSort(List<string> input)
+    {
+        Console.WriteLine("");
+    }
+        
+
+    // print max - case 5
+    static void PrintMax(List<string> input)
+    {
+        Console.WriteLine("");
+    }
+
+
+    // print min - case 6
+    static void PrintMin(List<string> input)
+    {
+        Console.WriteLine("");
+    }
+
+
+    // print average - case 7
+    static void PrintAverage(List<string> input)
+    {
+        Console.WriteLine("");
+    }
+
+
+    // print lengh of list - case 8
+    static void Printlen(List<string> input)
+    {
+        Console.WriteLine("");
+    }
+
+
+    //  print sum of list - case 9 
+    static void PrintSum(List<string> input)
+    {
+        Console.WriteLine("");
+    }
+
+
+    // convert to list 
+    static List<int> ConvertToList(string[] input)
+    {
+        return new List<int> (ConvertToList(input));
+    }
+
+
+    // convert to int 
+    static int ConverttoInt(string[] input)
+    {
+        return 1;
+    }
+
+
+
+}
+
